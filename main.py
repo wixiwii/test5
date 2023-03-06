ca7b8a15842572577bc5e12b9ec587315f02fecb
MIRA <wixiwii@gmail.com>
2023-03-06 16:56:08 +0400

    Add file

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+class Pet:
+   def __init__(self, name, species):
+       self.name = name
+       self.species = species
+       self.is_alive = True
+       self.age = 0
+   def grow(self):
+       self.age += 1
+cat = Pet("Мурзик", "кошка")
+cat.grow()
+print(f"{cat.name} растет и ему уже {cat.age} года")
 
-
-
-print('hello')
