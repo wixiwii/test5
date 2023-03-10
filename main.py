b0bb52185123e2fd576a7f2c34a3315a9d9dce55
MIRA <wixiwii@gmail.com>
2023-03-10 19:19:26 +0400

    Add file

--- main.py
+++ main.py
@@ -1,44 +1,20 @@
 import random
 
-class Person:
-    def __init__(self, name, age, health=100, state="idle"):
-        self.name = name
-        self.age = age
-        self.health = health
-        self.state = state
-        
-    def __str__(self):
-        return f"{self.name} ({self.age} year, health: {self.health}%, statistic: {self.state})"
-    
-    def work(self):
-        self.state = "working"
-        self.health -= random.randint(5, 15)
-        if self.health <= 0:
-            self.state = "death"
-    
-    def rest(self):
-        self.state = "rest"
-        self.health += random.randint(5, 15)
-        if self.health > 100:
-            self.health = 100
+class Encryptor:
+    def __init__(self, num):
+        self.num = num
+        self.__encrypt()
 
-class Simulation:
-    def __init__(self):
-        self.people = []
-    
-    def add_person(self, person):
-        self.people.append(person)
-    
-    def remove_person(self, person):
-        self.people.remove(person)
-    
-    def run(self, days):
-        for i in range(days):
-            print(f"\nDay {i + 1}")
-            for person in self.people:
-                action = random.choice(["work", "rest"])
-                if action == "work":
-                    person.work()
-                else:
-                    person.rest()
-                print(person)
+    def __encrypt(self):
+        operation = random.choice(["+", "-", "*", "/"])
+        if operation == "+":
+            self.num += random.randint(1, 10)
+        elif operation == "-":
+            self.num -= random.randint(1, 10)
+        elif operation == "*":
+            self.num *= random.randint(1, 5)
+        elif operation == "/":
+            self.num /= random.randint(1, 5)
+
+    def __str__(self):
+        return str(self.num)
