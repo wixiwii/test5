31a0cced9977911a59844fae9486b63f054a1b63
MIRA <wixiwii@gmail.com>
2023-03-22 12:53:14 +0400

    Add file

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-import turtle
-t = turtle.Turtle()
-t.pensize(10)
-t.forward(100)
-t.left(90)
-t.forward(100)
-t.penup()
-t.color('red', 'yellow')
-t.pendown()
-t.left(90)
-t.forward(100)
-t.left(180)
-t.forward(150)
+class Generator:
+    def __iter__(self):
+        return self.generator()
+
+    def generator(self):
+        for i in range(10):
+            yield i
+gen_iter = Generator()
+
+for i in gen_iter:
+    print(i)
