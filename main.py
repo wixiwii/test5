288f9a7a2a1e6018cc7dca73dcf3b45fda52b7b7
MIRA <wixiwii@gmail.com>
2023-03-31 16:17:35 +0400

    Add file

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-import time
+import sqlite3
+connection=sqlite3.connect("itstep_DB.sl3",5)
+cur = connection.cursor()
 
-def measure_time(func, *args, **kwargs):
-    start_time = time.time()
-    result = func(*args, **kwargs)
-    end_time = time.time()
-    print(f"Function {func.__name__} completed in {end_time - start_time} seconds(minutes).")
-    return result
-def slow_function(n):
-    total = 0
-    for i in range(n):
-        total += i
-        time.sleep(0.1)
-    return total
+cur.execute("CREATE TABLE first table (name TEXT);")
+connection.commit()
 
-result = measure_time(slow_function, 10)
+connection.close()
