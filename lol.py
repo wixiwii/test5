7d60f92d551a9140330bb7a6812a532810154397
MIRA <wixiwii@gmail.com>
2023-04-08 14:42:00 +0400

    Add file

--- lol.py
+++ lol.py
@@ -1,5 +1,9 @@
+import inspect
 import colorama
-dir(colorama)
 
+print(f"Attributes: {dir(colorama)}\n")
 
+print("Methods:")
+for name, data in inspect.getmembers(colorama, inspect.isfunction):
+    print(f"{name}: {data}")
 
