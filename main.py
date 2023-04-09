aa7e289baa8bb8cac71a45789ee8f4229f406111
MIRA <wixiwii@gmail.com>
2023-04-09 18:00:57 +0400

    Add file

--- main.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-import requests
-
-opener = urllib.request.build_opener()
-response = opener.open("https://httpbin.org/get")
-print(response.read())
-response = requests.get("https://httpbin.org/get")
-print(response.content)
