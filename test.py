2658623e57f75ed0ec2b0b1b5c6302cf8464c7ec
MIRA <wixiwii@gmail.com>
2023-04-10 19:54:24 +0400

    Add file

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
-import unittest
-from main import *
-
-class My_test(unittest.TestCase):
-    def test_args(self):
-        self.assertEqual(adder(2, 2), 4)
-
-if __name__ == "__test__":
-    unittest.test()
+import turtle
+t = turtle.Turtle()
+t.pensize(10)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.penup()
+t.color('red', 'yellow')
+t.pendown()
+t.forward(100)
+t.left(180)
+t.forward(150)
