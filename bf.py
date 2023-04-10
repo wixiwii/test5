4d927366c1d6e526227b4e54cae86c8ac6193350
MIRA <wixiwii@gmail.com>
2023-04-10 18:33:19 +0400

    Add file

--- bf.py
+++ bf.py
@@ -1,6 +1,22 @@
 import sqlite3
-connection = sqlite3.connect("srt_DB.sl3", 5)
-cur = connection.cursor()
-cur.execute("INSERT INTO first_table (name)VALUES ('Nick');")
-connection.commit()
-connection.close()
+
+conn = sqlite3.connect('test.db')
+cursor = conn.cursor()
+
+cursor.execute('CREATE TABLE cars (id INTEGER PRIMARY KEY, name TEXT)')
+
+cars = [
+    'Audi', 'BMW', 'Ford', 'Honda', 'Hyundai', 'Kia',
+    'Mercedes-Benz', 'Nissan', 'Toyota', 'Volkswagen'
+]
+for car in cars:
+    cursor.execute('INSERT INTO cars (name) VALUES (?)', (car,))
+
+cursor.execute('DELETE FROM cars WHERE name IN (?, ?)', ('Ford', 'Nissan'))
+
+cursor.execute('UPDATE cars SET name = ? WHERE name IN (?, ?, ?)', ('Chevrolet', 'Honda', 'Hyundai', 'Kia'))
+
+cursor.execute('DROP TABLE cars')
+
+conn.commit()
+conn.close()
